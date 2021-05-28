d517c7a00c29226471e3952b35f18e544d9a59f7
Pedro Higuera <higueragarzonpedroarturo@gmail.com>
2021-05-27 23:08:43 -0500

    Dark mode modified and improved

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,18 +1,31 @@
 var darkIcon = document.getElementById('dark_icon');
 var lightIcon = document.getElementById('light_icon');
 
-darkIcon.onclick = function(){
-    document.body.classList.toggle('dark_mode');
+darkIcon.onclick = darkTheme;
+lightIcon.onclick = lightTheme;
 
-        darkIcon.style.display='none';
-        lightIcon.style.display='block';
-    
+let localData = localStorage.getItem('theme');
+
+if(localData==null){
+    localStorage.setItem('theme','light');
+}else if(localData == 'light'){
+    lightTheme();
+    document.body.classList.remove('dark_mode');
+}else{
+    darkTheme();
+    document.body.classList.add('dark_mode');
 }
 
-lightIcon.onclick = function(){
+function lightTheme(){
     document.body.classList.toggle('dark_mode');
-    
-        darkIcon.style.display='block';
-        lightIcon.style.display='none';
+    darkIcon.style.display='block';
+    lightIcon.style.display='none';
+    localStorage.setItem('theme','light');
+}
 
+function darkTheme(){
+    document.body.classList.toggle('dark_mode');
+    darkIcon.style.display='none';
+    lightIcon.style.display='block';
+    localStorage.setItem('theme','dark');
 }
